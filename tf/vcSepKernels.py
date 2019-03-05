e35aae66c56ab6dcba74c8d194baeaf9986fa162
Valentin4869 <abc314@outlook.com>
2019-03-05 08:41:44 +0200

    Update vcSepKernels.py

--- tf/vcSepKernels.py
+++ tf/vcSepKernels.py
@@ -10,15 +10,14 @@ def imshow(im):
     matplotlib.pyplot.imshow(im);
     matplotlib.pyplot.show();
 
-#load original weights in matlab, save as .mat.
-#load .mat in python
-#find v and h for these weights.
-#save v and h as .mat
-#verify results of vh convolution in matlab
+#load original weights
+# define two nets: original architecture and sep architecture
+# 'train' for separable kernels and compare accuracy
+# loss is huge, but accuracy seems to recover early on even with large loss values.
 
 
 
-#exec(open("vcopt1.3.py").read())
+#exec(open("vcSepKernels.py").read())
 
 #-------------------------------------------------------------------------#
 #--------------------------Function Declarations--------------------------#
@@ -76,20 +75,24 @@ def max_pool_2x2(x):
 
 #today before leaving: load weights, define loss for each conv layer, find approximations, test accuracy with sep conv, batched
 
-#X_train, y_train, X_test, y_test = getCarData();
-X_train, y_train, X_test, y_test, X_validate, y_validate = getCarData2();
+X_train, y_train, X_test, y_test = getCarData();
+
 
 print('Constructing network\n');
 #tf.device('/cpu:0');
-epochs=8;
+epochs=1000;
 gridsearch=np.zeros([1,16]);
 
 glosses1=np.zeros([16,epochs])
 glosses2=np.zeros([16,epochs])
+gacc=np.zeros([16,epochs])
+
+
 train_N=np.shape(X_train)[0];
 minibatch_size=128;
 batches=int(np.ceil(train_N/minibatch_size));
 
+#next day: run pruning, record accuracy at each sparsity level for plot(sparsity/stage/accuracy)
 for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
 
     d=5;
@@ -116,6 +119,8 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
 
     W_conv1 = tf.Variable(np.load('weights/acc_977_972/W_conv1.npy'));
     W_conv2  =tf.Variable(np.load('weights/acc_977_972/W_conv2.npy'));
+    b_conv1  =tf.Variable(np.load('weights/acc_977_972/b_conv1.npy'));
+    b_conv2  =tf.Variable(np.load('weights/acc_977_972/b_conv2.npy'));
     W_d1 = tf.Variable(np.load('weights/acc_977_972/W_d1.npy'));
     b_d1 = tf.Variable(np.load('weights/acc_977_972/b_d1.npy'));
     W_d2 = tf.Variable(np.load('weights/acc_977_972/W_d2.npy'));
@@ -123,10 +128,10 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
     W_out = tf.Variable(np.load('weights/acc_977_972/W_out.npy'));
     b_out = tf.Variable(np.load('weights/acc_977_972/b_out.npy'));
 
-    h_conv1 = tf.nn.relu(conv2d(X, W_conv1)); #h_conv1 = tf.nn.relu(conv2d(X, W_conv1,) + b_conv1);
+    h_conv1 = tf.nn.relu(conv2d(X, W_conv1)+b_conv1); #h_conv1 = tf.nn.relu(conv2d(X, W_conv1,) + b_conv1);
     h_pool1 = max_pool_2x2(h_conv1);
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2));
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2)+b_conv2);
     h_pool2 = max_pool_2x2(h_conv2);
     h_pool2_flat = tf.reshape(h_pool2, [-1, 24*24*32]);
     h_d1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_d1) + b_d1);
@@ -177,7 +182,7 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
     ##loss
     y_softmax=tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=y_out);
     ce= tf.reduce_mean(y_softmax);
-    train_step = tf.train.RMSPropOptimizer(0.001).minimize(ce);
+    train_step = tf.train.RMSPropOptimizer(0.001,0.992).minimize(ce);
 
     y_true=tf.equal(tf.argmax(y_out,1), tf.argmax(y,1));
     accuracy = tf.reduce_mean(tf.cast(y_true, tf.float64));
@@ -195,9 +200,9 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
     #gloss=tf.reduce_sum(tf.reduce_sum(tf.norm(h_pool1- sph_pool1, axis=[1,2]),axis=0));
     #gloss2=tf.reduce_sum(tf.reduce_sum(tf.norm(h_pool2- sph_pool2, axis=[1,2]),axis=0));
 
-    gloss=tf.reduce_mean(tf.reduce_sum(tf.norm(h_pool1- sph_pool1, axis=[1,2]),axis=0));
-    gloss2=tf.reduce_mean(tf.reduce_sum(tf.norm(h_pool2- sph_pool2, axis=[1,2]),axis=0));
-
+    gloss=tf.reduce_mean((tf.norm(h_pool1- sph_pool1)));
+    gloss2=tf.reduce_mean((tf.norm(h_pool2- sph_pool2)));
+    
 
     #optsteph= tf.train.RMSPropOptimizer(0.001).minimize(gloss,var_list=[h1]);
     #optstepv= tf.train.RMSPropOptimizer(0.001).minimize(gloss,var_list=[v1]);
@@ -220,38 +225,39 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
     optsteph2= tf.train.RMSPropOptimizer(0.001,0.992).minimize(gloss2,var_list=[h2]);
     optstepv2= tf.train.RMSPropOptimizer(0.001,0.992).minimize(gloss2,var_list=[v2]);
 
+
+    sptrain_step1 = tf.train.RMSPropOptimizer(0.001,0.995).minimize(gloss,var_list=[h1,v1]);
+    sptrain_step2 = tf.train.RMSPropOptimizer(0.001,0.995).minimize(gloss2,var_list=[h2,v2]);
     hWf=np.load('hW_99.npy');
 
-    mean_test_acc=[];
-    mean_val_acc=[];
+
     session=Session();
     session.run(tf.global_variables_initializer()); 
 
+    mean_test_acc=[];
+    mean_val_acc=[];
     print('Initial original...')
 
-    for i in range(0,int(np.ceil( np.shape(y_validate)[0]/128.0))):    
-        mean_val_acc.append(accuracy.eval(session=session,feed_dict={X: X_validate[i*128:min( (i+1)*128,np.shape(y_validate)[0])], y: y_validate[i*128:min( (i+1)*128,np.shape(y_validate)[0])],keep_prob_c2d:1.0,keep_prob_d1d2:1.0}));
+    for i in range(0,300):    
+        mean_val_acc.append(accuracy.eval(session=session,feed_dict={X: X_test[i:i+1], y: y_test[i:i+1],keep_prob_c2d:1.0,keep_prob_d1d2:1.0}));
+    print('------ Mean validation accuracy(%i): %g ------' % (i,np.mean(mean_val_acc)));
 
-    print('------ Mean validation accuracy(%i): %g ------' % ( np.shape(y_validate)[0],np.mean(mean_val_acc)));
-
-    for i in range(0,np.shape(y_test)[0]):    
+    for i in range(300,np.shape(y_test)[0]):    
         mean_test_acc.append(accuracy.eval(session=session,feed_dict={X: X_test[i:i+1], y: y_test[i:i+1],keep_prob_c2d:1.0,keep_prob_d1d2:1.0}));
+    print('------ Mean test accuracy(%i): %g ------' % (i,np.mean(mean_test_acc)));
 
-    print('------ Mean test accuracy(%i): %g ------' % (np.shape(y_test)[0],np.mean(mean_test_acc)));
-   
 
     print('Initial sep...')
 
-    for i in range(0,int(np.ceil( np.shape(y_validate)[0]/128.0))):    
-        mean_val_acc.append(spaccuracy.eval(session=session,feed_dict={X: X_validate[i*128:min( (i+1)*128,np.shape(y_validate)[0])], y: y_validate[i*128:min( (i+1)*128,np.shape(y_validate)[0])],spkeep_prob_c2d:1.0,spkeep_prob_d1d2:1.0}));
+    for i in range(0,300):    
+        mean_val_acc.append(spaccuracy.eval(session=session,feed_dict={X: X_test[i:i+1], y: y_test[i:i+1],spkeep_prob_c2d:1.0,spkeep_prob_d1d2:1.0}));
+    print('------ Mean validation accuracy(%i): %g ------' % (i,np.mean(mean_val_acc)));
 
-    print('------ Mean validation accuracy(%i): %g ------' % ( np.shape(y_validate)[0],np.mean(mean_val_acc)));
-
-    for i in range(0,np.shape(y_test)[0]):    
+    for i in range(300,np.shape(y_test)[0]):    
         mean_test_acc.append(spaccuracy.eval(session=session,feed_dict={X: X_test[i:i+1], y: y_test[i:i+1],spkeep_prob_c2d:1.0,spkeep_prob_d1d2:1.0}));
+    print('------ Mean test accuracy(%i): %g ------' % (i,np.mean(mean_test_acc)));
 
-    print('------ Mean test accuracy(%i): %g ------' % (np.shape(y_test)[0],np.mean(mean_test_acc)));
-   
+
  
 
   
@@ -265,11 +271,15 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
 
         for batch in range(0, batches):
             
-        
-            optsteph.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
-            optstepv.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
-            optsteph2.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
-            optstepv2.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+
+            #optsteph.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+            #optstepv.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+            #optsteph2.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+            #optstepv2.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+
+            sptrain_step1.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+            sptrain_step2.run(feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]},session=session);
+
 
             epoch_loss1.append(session.run(gloss,feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]}))    
             epoch_loss2.append(session.run(gloss2,feed_dict={X:X_train[batch*minibatch_size:min((batch+1)*minibatch_size,train_N)]}))   
@@ -280,27 +290,32 @@ for K in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
                                                                         epoch_loss2[-1]));
 
                 
-        glosses1[K-1,e]=np.mean(epoch_loss1)
-        glosses2[K-1,e]=np.mean(epoch_loss2)
+        glosses1[K-1,e]=np.mean(epoch_loss1);
+        glosses2[K-1,e]=np.mean(epoch_loss2);
 
+        mean_test_acc=[];
+        mean_val_acc=[];
+        for i in range(0,300):    
+            mean_val_acc.append(spaccuracy.eval(session=session,feed_dict={X: X_test[i:i+1], y: y_test[i:i+1],spkeep_prob_c2d:1.0,spkeep_prob_d1d2:1.0}));
 
-        for i in range(0,int(np.ceil( np.shape(y_validate)[0]/128.0))):    
-            mean_val_acc.append(spaccuracy.eval(session=session,feed_dict={X: X_validate[i*128:min( (i+1)*128,np.shape(y_validate)[0])], y: y_validate[i*128:min( (i+1)*128,np.shape(y_validate)[0])],spkeep_prob_c2d:1.0,spkeep_prob_d1d2:1.0}));
+        print('------ Mean sep validation accuracy(%i): %g ------' % (i,np.mean(mean_val_acc)));
 
-        print('------ Mean validation accuracy(%i): %g ------' % ( np.shape(y_validate)[0],np.mean(mean_val_acc)));
-
-        for i in range(0,np.shape(y_test)[0]):    
+        for i in range(300,np.shape(y_test)[0]):    
             mean_test_acc.append(spaccuracy.eval(session=session,feed_dict={X: X_test[i:i+1], y: y_test[i:i+1],spkeep_prob_c2d:1.0,spkeep_prob_d1d2:1.0}));
 
-        print('------ Mean test accuracy(%i): %g ------' % (np.shape(y_test)[0],np.mean(mean_test_acc)));
-   
+        print('------ Mean sep test accuracy(%i): %g ------' % (i,np.mean(mean_test_acc)));
 
         print('------ Mean sep loss1: %g ------' % (glosses1[K-1,e]));
         print('------ Mean sep loss2: %g ------' % (glosses2[K-1,e]));
 
-        gridsearch[0,K-1]=  np.mean(mean_test_acc);  
+        gridsearch[0,K-1]=  (np.mean(mean_val_acc)+np.mean(mean_test_acc))/2.0;
+
+        gacc[K-1,e]=gridsearch[0,K-1];
+
     session.close();        
-    print(gridsearch)
+    print(gridsearch);
 
-print(gridsearch)
+print(gridsearch);
 
+imsave(np.reshape(glosses1,[1,16*epochs],'F'),'sclosses3/glosses1.csv')
+imsave(np.reshape(glosses2,[1,16*epochs],'F'),'sclosses3/glosses2.csv')
